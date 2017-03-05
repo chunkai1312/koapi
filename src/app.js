0040c0fc2acac9ba97cc1b429ede6023fb6a0182
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-06 03:50:39 +0800

    refactor: app

--- src/app.js
+++ src/app.js
@@ -2,14 +2,11 @@ import Koa from 'koa'
 import middlewares from './middlewares'
 import routes from './routes'
 import connectMongoDB from './config/mongoose'
-import config from './config'
 
 connectMongoDB()
 
 const app = new Koa()
   .use(middlewares())
   .use(routes())
-
-app.listen(config.port, () => console.log('Server is running on port %d', config.port))
 
 export default app
