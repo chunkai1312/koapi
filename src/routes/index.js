b37a9137912502461d4cccbe074bc9d0a2c49ccf
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-04 03:02:15 +0800

    refactor: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import Router from 'koa-router'
 import { compose } from 'koa-convert'
-import { todo } from '../controllers'
+import { Todo } from '../controllers'
 
 const router = new Router({ prefix: '/api' })
-  .get('/todos', todo.index)
-  .post('/todos', todo.create)
-  .get('/todos/:id', todo.show)
-  .put('/todos/:id', todo.update)
-  .delete('/todos/:id', todo.destroy)
+  .get('/todos', Todo.index)
+  .post('/todos', Todo.create)
+  .get('/todos/:id', Todo.show)
+  .put('/todos/:id', Todo.update)
+  .delete('/todos/:id', Todo.destroy)
 
 export default () => compose([
   router.routes(),
