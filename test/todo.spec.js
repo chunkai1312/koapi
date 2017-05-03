d40fc2527bf53847381776f3c31f200dad44f37f
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-04 04:18:04 +0800

    test: todo

--- test/todo.spec.js
+++ test/todo.spec.js
@@ -7,7 +7,7 @@ describe('Todo API:', () => {
   let todo
 
   describe('GET /api/todos', () => {
-    it('should respond with JSON array', () => {
+    it('should respond 200 with JSON array', () => {
       return request(server)
         .get('/api/todos')
         .expect('Content-Type', /json/)
@@ -17,7 +17,7 @@ describe('Todo API:', () => {
   })
 
   describe('POST /api/todos', () => {
-    it('should respond with the newly created todo', () => {
+    it('should respond 200 with the newly created todo', () => {
       return request(server)
         .post('/api/todos')
         .send({
@@ -35,7 +35,7 @@ describe('Todo API:', () => {
   })
 
   describe('GET /api/todos/:id', () => {
-    it('should respond with the requested todo', () => {
+    it('should respond 200 with the requested todo', () => {
       return request(server)
         .get(`/api/todos/${todo.id}`)
         .expect('Content-Type', /json/)
@@ -49,7 +49,7 @@ describe('Todo API:', () => {
   })
 
   describe('PUT /api/todos/:id', () => {
-    it('should respond with the updated todo', () => {
+    it('should respond 200 with the updated todo', () => {
       return request(server)
         .put(`/api/todos/${todo.id}`)
         .send({
@@ -67,13 +67,17 @@ describe('Todo API:', () => {
   })
 
   describe('DELETE /api/todos/:id', () => {
-    it('should respond with 204 on successful removal', () => {
+    it('should respond 200 with the deleted id', () => {
       return request(server)
         .delete(`/api/todos/${todo.id}`)
-        .expect(204)
+        .expect(200)
+        .then(res => {
+          expect(res.body.id).toBe(todo.id)
+          expect(res.body.deleted).toBe(true)
+        })
     })
 
-    it('should respond with 404 when todo does not exist', () => {
+    it('should respond with 404 when the todo does not exist', () => {
       return request(server)
         .delete(`/api/todos/${todo.id}`)
         .expect(404)
