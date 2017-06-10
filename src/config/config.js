b6eb4d4732730f59c3a03921bb75a6e258b67f6e
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-11 04:26:41 +0800

    refactor: config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,11 @@
+import path from 'path'
+
 export default {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 8080,
+  env: process.env.NODE_ENV,
+  host: process.env.HOST,
+  port: process.env.PORT,
+
+  context: path.normalize(`${__dirname}/..`),
 
   mongoDB: {
     uri: process.env.MONGODB_URI,
--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
-import { mongoDB } from '../config'
+import glob from 'glob'
+import config from '../config'
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(mongoDB.uri, mongoDB.options)
+mongoose.connect(config.mongoDB.uri, config.mongoDB.options)
 
 mongoose.connection
-  .on('connected', () => console.log(`Mongoose default connection open to ${mongoDB.uri}`))
+  .on('connected', () => console.log(`Mongoose default connection open to ${config.mongoDB.uri}`))
   .on('disconnected', () => console.log('Mongoose default connection disconnected'))
   .on('error', (err) => console.error(`Mongoose default connection error: ${err}`))
 
@@ -16,3 +17,6 @@ process.on('SIGINT', () => {
     process.exit(0)
   })
 })
+
+const models = glob.sync(`${config.context}/models/*.js`)
+models.forEach(model => require(model))
