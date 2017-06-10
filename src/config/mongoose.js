5bd8f0c2abdeebe95747ddf5df29c49b965ddd82
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-11 03:13:09 +0800

    refactor(config): mongoose

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,20 +1,18 @@
 import mongoose from 'mongoose'
-import config from '../config'
+import { mongoDB } from '../config'
 
 mongoose.Promise = global.Promise
 
-export default function connectMongoDB () {
-  mongoose.connect(config.mongoDB.uri, config.mongoDB.options)
+mongoose.connect(mongoDB.uri, mongoDB.options)
 
-  mongoose.connection
-    .on('connected', () => console.log(`Mongoose connection open to ${config.mongoDB.uri}`))
-    .on('disconnected', () => console.log('Mongoose connection disconnected'))
-    .on('error', (err) => console.error(`Mongoose connection error: ${err}`))
+mongoose.connection
+  .on('connected', () => console.log(`Mongoose default connection open to ${mongoDB.uri}`))
+  .on('disconnected', () => console.log('Mongoose default connection disconnected'))
+  .on('error', (err) => console.error(`Mongoose default connection error: ${err}`))
 
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      console.log('Mongoose connection disconnected through app termination')
-      process.exit(0)
-    })
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination')
+    process.exit(0)
   })
-}
+})
