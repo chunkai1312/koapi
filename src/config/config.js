871d15aabe44e871d3f3498cb1a4cb6b91b37b96
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-11 03:12:34 +0800

    refactor: config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,9 @@
-import path from 'path'
-
 export default {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
-  root: path.normalize(`${__dirname}/../..`)
+
+  mongoDB: {
+    uri: process.env.MONGODB_URI,
+    options: {}
+  }
 }
