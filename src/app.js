7bc360b91348443187bb355e731c988e9a4bf1e8
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-11 16:43:49 +0800

    refactor: app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import Koa from 'koa'
 import middlewares from './middlewares'
 import routes from './routes'
+import logger from './config/logger'
 import './config/mongoose'
 
 const app = new Koa()
   .use(middlewares())
   .use(routes())
+
+app.on('error', (err, ctx) =>
+  logger.error('server error', err, ctx)
+)
 
 export default app
